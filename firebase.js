3086afd83d5cd78e189423d9e6f49d0655afe380
ligagoianiafutebol-code <ligagoianiafutebol@gmail.com>
2026-04-16 21:17:17 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCyArj4yRlEl-yibkc0VEmL_4UJpRHHQSc",
   authDomain: "liga-goiana-base.firebaseapp.com",
   projectId: "liga-goiana-base",
-  storageBucket: "liga-goiana-base.firebasestorage.app",
+  storageBucket: "liga-goiana-base.appspot.com",
   messagingSenderId: "952863591183",
   appId: "1:952863591183:web:4eea34f494ba2dd754cc7a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
